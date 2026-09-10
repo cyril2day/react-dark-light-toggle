91a0fb7b612bf64fdf60569c568d3540c49d760c
psi <cy.lonsido@gmail.com>
2026-09-11 00:21:33 +0800

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,25 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const basePath = '/react-dark-light-toggle/'
+
+function rewriteImagePaths() {
+  return {
+    name: 'rewrite-image-paths',
+    enforce: 'post',
+    generateBundle(_, bundle) {
+      for (const file of Object.values(bundle)) {
+        if (file.type === 'chunk' && file.code) {
+          file.code = file.code
+            .replace(/(["'`])\/images\//g, `$1${basePath}images/`)
+            .replace(/(["'`])images\//g, `$1${basePath}images/`)
+        }
+      }
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: basePath,
+  plugins: [react(), tailwindcss(), rewriteImagePaths()],
 })
