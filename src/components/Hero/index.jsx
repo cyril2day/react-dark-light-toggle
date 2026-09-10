3e14d1bc7b61c47d11e2be40190f37c7aee5bd56
psi <cy.lonsido@gmail.com>
2026-09-10 21:59:52 +0800

    Build the hero section

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,12 +1,54 @@
 const Hero = () => {
   return (
-    <section className='min-h-screen pt-24'>
-      <h1>Hero Section.</h1>
-      <p>
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.    
+    <section className='min-h-screen pt-24 flex flex-col md:flex-row items-center justify-between dark:bg-gray-800 px-6 md:px-20 py-10'>
+      {/* Left Side: Text */}
+      <div className='max-w-lg space-y-4'>
+        <p className='text-lg'>👋 Hello, I am</p>
+        <h1 className='text-4xl md:text-6xl font-bold text-blue-600 dark:text-blue-400'>
+          Simba 
+        </h1>
+        <p className="text-gray-600 dark:text-gray-300 text-xl">
+          Web Developer / Freelancer
+        </p>
 
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-      </p>
+        <div className='flex gap-4'>
+          <a
+            href='#contact'
+            className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+          >Let's Connect</a>
+          <a
+            href='#cv'
+            className='px-4 py-2 border border-blue-500 rounded text-blue-500 hover:bg-blue-500 hover:text-white'
+          >Download CV</a>
+        </div>
+      </div>
+
+      {/* Right Side: Image + Icons */}
+      <div className='relative mt-10 md:mt-0'>
+        <img 
+          src='/images/hero/lion.jpg'
+          alt='profile'
+          className='rounded-full shadow-lg'
+          width={300}
+          height={300}
+        />
+
+        <div className='absolute top-[-20px] right-[-20px] bg-white dark:bg-gray-700 p-2 rounded-full shadow-md'>
+          <img 
+            src='/images/hero/flutter.svg'
+            alt='Flutter'
+            className='w-8 h-8'
+          />
+        </div>
+
+        <div className='absolute bottom-[-20px] left-[-20px] bg-white dark:bg-gray-700 p-2 rounded-full shadow-md'>
+          <img 
+            src='/images/hero/convex.svg'
+            alt='Convex'
+            className='w-8 h-8'
+          />
+        </div>
+      </div>
     </section>
   )
 }
