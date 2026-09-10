d7709a797ddb471d44c10cf11f064482f3a83a81
psi <cy.lonsido@gmail.com>
2026-09-10 22:31:15 +0800

    Setup the skill and experience section layout

--- src/components/SkillsExperienceSection/index.jsx
+++ src/components/SkillsExperienceSection/index.jsx
@@ -3,12 +3,27 @@ import SkillCard from './SkillCard'
 
 const SkillsExperienceSection = () => {
   return (
-    <section className='min-h-screen py-20'>
-      <h1>SkillsExperienceSection Section.</h1>
-      <p>
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-      </p>
-    </section>
+  <section className='min-h-screen py-20'>
+    <h2 className='text-3xl font-semibold text-center'>
+      Skills & Experience
+    </h2>
+
+    <h3 className='text-4xl font-bold text-center mb-16 text-blue-600 dark:text-blue-400'>
+      What I Do & Where I've Been
+    </h3>
+
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-12 max-2-6xl mx-auto'>
+      {/* Skills */}
+      <div>
+        <h4 className='text-2xl font-semibold mb-4'>SKills</h4>
+      </div>
+
+      {/* Experience Timeline */}
+      <div>
+        <h4 className='text-2xl font-semibold mb-4'>Experience</h4>
+      </div>
+    </div>
+  </section>
   )
 }
 
